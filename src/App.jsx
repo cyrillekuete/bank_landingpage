2a2949a426a5b052748bb1b873dda7b9bb11e821
Kuete Cyrille <cyrillekuete@outlook.com>
2023-02-11 04:33:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Welcome to REact JS with Vite</h1>
+    </div>
+  );
+};
+
+export default App;
